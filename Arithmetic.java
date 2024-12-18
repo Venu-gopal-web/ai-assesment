75563b905b73339c232cbdc53cc32f2efe9c6cab
Venu-gopal-web <venu.devara-konda@yorksj.ac.uk>
2024-12-18 23:42:44 +0000

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+package javapractise;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dvenu
+ *
+ */
+module javapractise {
+}
